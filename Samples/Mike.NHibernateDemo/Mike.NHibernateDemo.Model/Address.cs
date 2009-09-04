44661eaff0f14cf15e8c8af3708529561de441a8
mikehadlow <mikehadlow@6e4e2a5c-a03d-11dd-932a-8f5888711c47>
2009-09-04 15:14:34 +0000

    More Demos, Added Address and a class diagram.

---
+++ Samples/Mike.NHibernateDemo/Mike.NHibernateDemo.Model/Address.cs
@@ -0,0 +1,8 @@
+namespace Mike.NHibernateDemo.Model
+{
+    public class Address
+    {
+        public virtual string Line1 { get; set; }
+        public virtual string Town { get; set; }
+    }
+}
